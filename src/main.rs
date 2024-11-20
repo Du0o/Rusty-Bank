e0e0fe373600668bd152af21642bd9fe0dae7e3c
SaltyIce <todorovteddy@gmail.com>
2024-11-19 21:22:03 -0800

    just starting

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
 fn main() {
-    println!("Hello rust from cargo!");
+    println!(r"
+_____________________________________________________________________________________
+ ________                __                  _______                       __       
+|        \              |  \                |       \                     |  \      
+ \$$$$$$$$______    ____| $$  _______       | $$$$$$$\  ______   _______  | $$   __ 
+   | $$  /      \  /      $$ /       \      | $$__/ $$ |      \ |       \ | $$  /  \
+   | $$ |  $$$$$$\|  $$$$$$$|  $$$$$$$      | $$    $$  \$$$$$$\| $$$$$$$\| $$_/  $$
+   | $$ | $$    $$| $$  | $$ \$$    \       | $$$$$$$\ /      $$| $$  | $$| $$   $$ 
+   | $$ | $$$$$$$$| $$__| $$ _\$$$$$$\      | $$__/ $$|  $$$$$$$| $$  | $$| $$$$$$\ 
+   | $$  \$$     \ \$$    $$|       $$      | $$    $$ \$$    $$| $$  | $$| $$  \$$\
+    \$$   \$$$$$$$  \$$$$$$$ \$$$$$$$        \$$$$$$$   \$$$$$$$ \$$   \$$ \$$   \$$
+_____________________________________________________________________________________
+    ");
+    println!("Welcome to the Rusty bank!");
+
+    let mut fname: String = String::new();
+    let mut lname: String = String::new();
+    let mut passphrase: String = String::new();
+
+    println!("Please enter your first name: ");
+    std::io::stdin().read_line(&mut fname).expect("error not found");  
+
+    println!("Please enter your last name: ");
+    std::io::stdin().read_line(&mut lname).expect("error not found"); 
+    
+    println!("Please enter your password: ");
+    std::io::stdin().read_line(&mut passphrase).expect("error not found");  
+    
 }
